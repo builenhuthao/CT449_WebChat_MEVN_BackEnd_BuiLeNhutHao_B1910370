2ab667494c20dce796693007450b55bdc03f2f39
builenhuthao <haoiuthi1402@gmail.com>
2023-04-25 19:05:52 +0700

    Hoan thanh du an

--- server.js
+++ server.js
@@ -2,22 +2,10 @@ const express = require("express");
 
 const app = express();
 
-const http = require("http").createServer(app);
+const auth = require("./modules/auth");
 
-var mongodb = require("mongodb");
-
-var MongoClient = mongodb.MongoClient;
-
-var ObjectId = mongodb.ObjectId;
-
-const expressFormidable = require("express-formidable");
-app.use(expressFormidable());
-
-const bcrypt = require("bcrypt");
-
-const jwt = require("jsonwebtoken");
-
-const jwtSecret = "jwtSecret1234567890";
+const contact = require("./modules/contact");
+const chat = require("./modules/chat");
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -33,6 +21,39 @@ app.use(function (req, res, next) {
   next();
 });
 
+const http = require("http").createServer(app);
+
+var mongodb = require("mongodb");
+
+var MongoClient = mongodb.MongoClient;
+
+var ObjectId = mongodb.ObjectId;
+
+const expressFormidable = require("express-formidable");
+
+app.use(expressFormidable());
+
+const fileSystem = require("fs");
+
+app.use("/uploads/profiles", express.static(__dirname + "/uploads/profiles"));
+
+const bcrypt = require("bcryptjs");
+
+const jwt = require("jsonwebtoken");
+
+const jwtSecret = "jwtSecret1234567890";
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: ["http://localhost:8080", "http://172.20.10.4:8080"],
+  },
+});
+
+const apiURL = "http://localhost:3000";
+const mainURL = "http://localhost:8080";
+
+global.users = [];
+
 http.listen(process.env.PORT || 3000, function () {
   console.log("Server has been started at: " + (process.env.PORT || 3000));
 
@@ -41,16 +62,209 @@ http.listen(process.env.PORT || 3000, function () {
       console.error(error);
       return;
     }
+
     db = client.db("mevn_chat_app");
     global.db = db;
     console.log("Database connected");
 
+    socketIO.on("connection", function (socket) {
+      socket.on("connected", function (email) {
+        global.users[email] = socket.id;
+      });
+    });
+
+    contact.init(app, express);
+    chat.init(app, express);
+    chat.socketIO = socketIO;
+
+    app.post("/updateProfile", auth, async function (request, result) {
+      const user = request.user;
+      const name = request.fields.name;
+      const picture = request.files.picture;
+      const previousPicture = user.picture;
+
+      let picturePath = "";
+      if (picture != null && picture.size > 0) {
+        if (picture.type == "image/jpeg" || picture.type == "image/png") {
+          picturePath =
+            "uploads/profiles/" + user.email.split(".")[0] + "-" + picture.name;
+
+          fileSystem.readFile(picture.path, function (error, data) {
+            if (error) {
+              console.error(error);
+              return;
+            }
+
+            fileSystem.writeFile(picturePath, data, function (error) {
+              if (error) {
+                console.error(error);
+                return;
+              }
+
+              if (previousPicture) {
+                fileSystem.unlink(previousPicture, function (error) {
+                  if (error) {
+                    console.error(error);
+                  }
+                });
+              }
+            });
+
+            fileSystem.unlink(picture.path, function (error) {
+              if (error) {
+                console.error(error);
+              }
+            });
+          });
+        }
+      }
+
+      await db.collection("users").findOneAndUpdate(
+        {
+          _id: user._id,
+        },
+        {
+          $set: {
+            name: name,
+            picture: picturePath,
+          },
+        }
+      );
+
+      user.name = name;
+      user.picture = picturePath;
+
+      delete user.password;
+      delete user.accessToken;
+      delete user.createdAt;
+      delete user.verifiedAt;
+      delete user.verificationCode;
+      delete user.resetToken;
+
+      result.json({
+        status: "success",
+        message: "Profile has been updated.",
+        user: user,
+      });
+    });
+
+    app.post("/deleteNotification", auth, async function (request, result) {
+      const user = request.user;
+
+      const _id = request.fields._id;
+
+      await db.collection("users").findOneAndUpdate(
+        {
+          _id: user._id,
+        },
+        {
+          $pull: {
+            notifications: {
+              _id: ObjectId(_id),
+            },
+          },
+        }
+      );
+
+      result.json({
+        status: "success",
+        message: "Notification has been removed.",
+      });
+    });
+
+    app.post(
+      "/markNotificationsAsRead",
+      auth,
+      async function (request, result) {
+        const user = request.user;
+
+        await db.collection("users").findOneAndUpdate(
+          {
+            _id: user._id,
+          },
+          {
+            $set: {
+              "notifications.$[].isRead": true,
+            },
+          }
+        );
+
+        result.json({
+          status: "success",
+          message: "Notification has been marked as read.",
+        });
+      }
+    );
+
+    app.post("/search", auth, async function (request, result) {
+      // get authenticated user
+      const user = request.user;
+
+      const query = request.fields.query;
+
+      const contacts = [];
+
+      for (let a = 0; a < user.contacts.length; a++) {
+        if (
+          user.contacts[a].name.includes(query) ||
+          user.contacts[a].email.includes(query)
+        ) {
+          contacts.push(user.contacts[a]);
+        }
+      }
+
+      result.json({
+        status: "success",
+        message: "Data has been fetched.",
+        contacts: contacts,
+      });
+    });
+
+    app.post("/logout", auth, async function (request, result) {
+      const user = request.user;
+
+      await db.collection("users").findOneAndUpdate(
+        {
+          _id: user._id,
+        },
+        {
+          $set: {
+            accessToken: "",
+          },
+        }
+      );
+
+      result.json({
+        status: "success",
+        message: "Logout successfully.",
+      });
+    });
+
+    app.post("/getUser", auth, async function (request, result) {
+      const user = request.user;
+
+      let unreadNotifications = 0;
+      if (user.notifications) {
+        for (let a = 0; a < user.notifications.length; a++) {
+          if (!user.notifications[a].isRead) {
+            unreadNotifications++;
+          }
+        }
+      }
+
+      result.json({
+        status: "success",
+        message: "Data has been fetched.",
+        user: user,
+
+        unreadNotifications: unreadNotifications,
+      });
+    });
+
     app.post("/login", async function (request, result) {
-      // get values from login form
       const email = request.fields.email;
       const password = request.fields.password;
 
-      // check if email exists
       const user = await db.collection("users").findOne({
         email: email,
       });
@@ -71,10 +285,8 @@ http.listen(process.env.PORT || 3000, function () {
         return;
       }
 
-      // check if password is correct
-      bcrypt.compare(password, user.password, async function (error, isVerify) {
-        if (isVerify) {
-          // generate JWT of user
+      bcrypt.compare(password, user.password, async function (err, res) {
+        if (res === true) {
           const accessToken = jwt.sign(
             {
               userId: user._id.toString(),
@@ -82,7 +294,6 @@ http.listen(process.env.PORT || 3000, function () {
             jwtSecret
           );
 
-          // update JWT of user in database
           await db.collection("users").findOneAndUpdate(
             {
               email: email,
@@ -108,6 +319,38 @@ http.listen(process.env.PORT || 3000, function () {
           message: "Password is not correct.",
         });
       });
+
+      /*bcrypt.compare(password, user.password, async function (error, isVerify) {
+                if (isVerify) {
+
+                    // generate JWT of user
+                    const accessToken = jwt.sign({
+                        "userId": user._id.toString()
+                    }, jwtSecret);
+
+                    // update JWT of user in database
+                    await db.collection("users").findOneAndUpdate({
+                        "email": email
+                    }, {
+                        $set: {
+                            "accessToken": accessToken
+                        }
+                    });
+
+                    result.json({
+                        status: "success",
+                        message: "Login successfully.",
+                        accessToken: accessToken
+                    });
+
+                    return;
+                }
+
+                result.json({
+                    "status": "error",
+                    "message": "Password is not correct."
+                });
+            });*/
     });
 
     app.post("/registration", async function (request, result) {
@@ -136,23 +379,47 @@ http.listen(process.env.PORT || 3000, function () {
         return;
       }
 
-      bcrypt.hash(password, 10, async function (error, hash) {
-        // insert in database
-        await db.collection("users").insertOne({
-          name: name,
-          email: email,
-          password: hash,
-          accessToken: "",
-          contacts: [],
-          notifications: [],
-          createdAt: createdAt,
-        });
+      bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(password, salt, async function (err, hash) {
+          // insert in database
+          await db.collection("users").insertOne({
+            name: name,
+            email: email,
+            password: hash,
+            picture: "",
+            accessToken: "",
+            contacts: [],
+            notifications: [],
+            verifiedAt: new Date().getTime(),
+            resetToken: "",
+            createdAt: createdAt,
+          });
 
-        result.status(200).json({
-          status: "success",
-          message: "User has been signed up.",
+          result.status(200).json({
+            status: "success",
+            message: "User has been signed up.",
+          });
         });
       });
+
+      /*bcrypt.hash(password, 10, async function (error, hash) {
+
+                // insert in database
+                await db.collection("users").insertOne({
+                    name: name,
+                    email: email,
+                    password: hash,
+                    accessToken: "",
+                    contacts: [],
+                    notifications: [],
+                    createdAt: createdAt
+                });
+
+                result.status(200).json({
+                    status: "success",
+                    message: "User has been signed up."
+                });
+            });*/
     });
   });
 });
